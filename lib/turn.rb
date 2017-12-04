7209f90c48bc3871bff9623a8a2794b685d1c9f9
Erica Murdock-Waters <ericahpa@gmail.com>
2017-12-04 22:41:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,6 +11,13 @@
 #end
 
 
+# #display_board accepts a board and prints
+# out the current state.
+board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
 def display_board(board)
-  puts
-end   
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end 
