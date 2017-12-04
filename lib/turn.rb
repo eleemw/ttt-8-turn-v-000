f7915a8a5dfd6128c9dcfb67f85f90ba50316b9d
Erica Murdock-Waters <ericahpa@gmail.com>
2017-12-04 22:31:08 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+# ask for input 1-9
+
+# receive user input
+# convert input to an index
+
+# index is valid
+   # make move for index
+   # show board
+# else
+  # ask for input again until you get a valid input
+#end     
